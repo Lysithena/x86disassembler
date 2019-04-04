f4ef2916b8f504f7f1be32ced034373bc74bf252
Lysithena <gotti0916@gmail.com>
2019-04-05 03:45:54 +0900

    なんとか動く？displacementの出力とコードの重複箇所をなんとかします

--- disassmble.go
+++ disassmble.go
@@ -1,4 +1,4 @@
-package x86disassembler
+package main
 
 import (
 	"fmt"
@@ -7,15 +7,36 @@ import (
 
 type modRM struct {
 	mod   byte
-	R     byte
-	M     byte
-	Rname string
-	Mname string
+	r     byte
+	m     byte
+	rname string
+	mname string
+}
+type sib struct {
+	ss    byte
+	index byte
+	base  byte
+}
+type linearAddress struct {
+	segmentRegsiter string
+	displacement    string
+	indexRegister   string
+	scale           byte
+}
+type instruction struct {
+	opcode       byte
+	opcodeString string
+	modrm        byte
+	modrmStruct  modRM
+	sib          byte
+	sibStruct    sib
+	address      linearAddress
+	immediate    string
 }
 
-func analyzeModRMOfCalc(buffer []byte) (mnemonic string) {
+func selectTypeOfCalc(buffer byte) (mnemonic string) {
 
-	switch buffer[0] >> 3 & 0x7 { //opcode
+	switch buffer >> 3 & 0x7 { //opcode
 	case 0x0:
 		mnemonic += "add"
 		break
@@ -41,123 +62,50 @@ func analyzeModRMOfCalc(buffer []byte) (mnemonic string) {
 		mnemonic += "cmp"
 		break
 	}
-	mnemonic += " "
-	modrm := analyzeModRM(buffer)
-	mnemonic += modrm.Mname + " "
 	return mnemonic
+}
+func numberToRegister(buffer byte) (ret string) {
+	switch buffer {
+	case 0x0:
+		ret = "eax"
+		break
+	case 0x1:
+		ret = "ecx"
+		break
+	case 0x2:
+		ret = "edx"
+		break
+	case 0x3:
+		ret = "ebx"
+		break
+	case 0x4:
+		ret = "esp"
+		break
+	case 0x5:
+		ret = "ebp"
+		break
+	case 0x6:
+		ret = "esi"
+		break
+	case 0x7:
+		ret = "edi"
+		break
+	default:
+		break
+	}
+	return ret
 }
 
-func analyzeModRM(buffer []byte) modRM {
+func analyzeModRM(buffer byte) modRM {
 	var modrm modRM
-	modrm.mod = buffer[0] >> 6
-	modrm.R = buffer[0] >> 3 & 0x7
-	modrm.M = buffer[0] & 0x7
-	switch buffer[0] >> 3 & 0x7 {
-	case 0x0:
-		modrm.Rname = "eax"
-		break
-	case 0x1:
-		modrm.Rname = "ecx"
-		break
-	case 0x2:
-		modrm.Rname = "edx"
-		break
-	case 0x3:
-		modrm.Rname = "ebx"
-		break
-	case 0x4:
-		modrm.Rname = "esp"
-		break
-	case 0x5:
-		modrm.Rname = "ebp"
-		break
-	case 0x6:
-		modrm.Rname = "esi"
-		break
-	case 0x7:
-		modrm.Rname = "edi"
-		break
-	}
-	switch buffer[0] & 0x7 {
-	case 0x0:
-		modrm.Mname = "eax"
-		break
-	case 0x1:
-		modrm.Mname = "ecx"
-		break
-	case 0x2:
-		modrm.Mname = "edx"
-		break
-	case 0x3:
-		modrm.Mname = "ebx"
-		break
-	case 0x4:
-		modrm.Mname = "esp"
-		break
-	case 0x5:
-		modrm.Mname = "ebp"
-		break
-	case 0x6:
-		modrm.Mname = "esi"
-		break
-	case 0x7:
-		modrm.Mname = "edi"
-		break
-	}
+	modrm.mod = buffer >> 6
+	modrm.r = buffer >> 3 & 0x7
+	modrm.m = buffer & 0x7
+	modrm.rname = numberToRegister(buffer >> 3 & 0x7)
+	modrm.mname = numberToRegister(buffer & 0x7)
 	return modrm
 }
-func getArgumentsizeFromMod(modrm modRM) (size byte) {
-	switch modrm.mod {
-	case 0x01:
-		return 1
-	case 0x2:
-		return 4
-	default:
-		return 0
-	}
-}
-func getMnemonicFromMod(modrm modRM, argument []byte, mode bool) (mnemonic string) {
-	if mode {
-		switch modrm.mod {
-		case 0x00:
-			mnemonic += "[" + modrm.Rname + "], " + modrm.Mname
-			break
-		case 0x01:
-		case 0x02:
-			mnemonic += "[" + modrm.Rname + " +0x" + string(argument) + "], " + modrm.Mname
-			break
-		case 0x03:
-			mnemonic += modrm.Rname + ", " + modrm.Mname
-			break
-		default:
-			mnemonic += "Error"
-		}
-		return mnemonic
-	} else {
-		switch modrm.mod {
-		case 0x00:
-			mnemonic += modrm.Rname + ", [" + modrm.Mname + "]"
-			break
-		case 0x01:
-		case 0x02:
-			mnemonic += modrm.Rname + ", [" + modrm.Mname + " +0x" + string(argument) + "]"
-			break
-		case 0x03:
-			mnemonic += modrm.Rname + ", " + modrm.Mname
-			break
-		default:
-			mnemonic += "Error"
-		}
-		return mnemonic
-	}
-}
-func getMnemonicFromType(buffer []byte, file *os.File, mode bool) (mnemonic string) {
-	modrm := analyzeModRM(buffer)
-	var temp = make([]byte, getArgumentsizeFromMod(modrm))
-	file.Read(temp)
-	mnemonic += getMnemonicFromMod(modrm, temp, mode)
-	return mnemonic
-}
+
 func main() {
 	filename := os.Args[1]
 	file, err := os.Open(filename)
@@ -166,50 +114,189 @@ func main() {
 	}
 
 	for {
-		var buffer = make([]byte, 1)
-		len, err := file.Read(buffer)
+		//var opcode, modrm, sib byte
+		//var displacement, immediate string
+
+		var instruction instruction
+		var buffer8 = make([]byte, 1)
+		len, err := file.Read(buffer8)
+		instruction.opcode = buffer8[0]
+
 		if err != nil {
 			fmt.Println(err)
 		}
 		if len == 0 {
 			break
 		}
-		var mnemonic string
 
-		switch buffer[0] {
-		case 0x01:
-			mnemonic += "add "
-			file.Read(buffer)
-			getMnemonicFromType(buffer, file, true)
+		switch instruction.opcode {
+		case 0x50:
+		case 0x51:
+		case 0x52:
+		case 0x53:
+		case 0x54:
+		case 0x55:
+		case 0x56:
+		case 0x57:
+			instruction.opcodeString = "push"
+			instruction.modrmStruct.rname = numberToRegister(instruction.opcode - 0x50)
+			fmt.Printf("%s %s\n", instruction.opcodeString, instruction.modrmStruct.rname)
+			break
 		case 0x81:
-			mnemonic += "calc- "
-			file.Read(buffer)
-			mnemonic += analyzeModRMOfCalc(buffer)
-			for i := 0; i < 4; i++ {
-				file.Read(buffer)
-				mnemonic += string(buffer)
+			file.Read(buffer8)
+			instruction.modrm = buffer8[0]
+			instruction.opcodeString = selectTypeOfCalc(instruction.modrm)
+			instruction.modrmStruct = analyzeModRM(instruction.modrm)
+			instruction.address.segmentRegsiter = instruction.modrmStruct.mname
+			var buffer32 = make([]byte, 4)
+			var buffer = ""
+			switch instruction.modrmStruct.mod {
+			case 0x0:
+				buffer = "[" + instruction.address.segmentRegsiter + "]"
+			case 0x1:
+				file.Read(buffer8)
+				instruction.address.displacement = string(buffer8)
+				buffer = "[" + instruction.address.segmentRegsiter + "+0x" + instruction.address.displacement + "]"
+				break
+			case 0x2:
+				file.Read(buffer32)
+				instruction.address.displacement = string(buffer32)
+				buffer = "[" + instruction.address.segmentRegsiter + "+0x" + instruction.address.displacement + "]"
+				break
+			case 0x3:
+				buffer = instruction.address.segmentRegsiter
+			default:
+				break
 			}
+
+			file.Read(buffer32)
+			instruction.immediate = string(buffer32)
+			fmt.Printf("%s %s, %s", instruction.opcodeString, buffer, instruction.immediate)
 			break
+
 		case 0x83:
-			mnemonic += "calc-"
-			file.Read(buffer)
-			mnemonic += analyzeModRMOfCalc(buffer)
-			file.Read(buffer)
-			mnemonic += string(buffer)
+			file.Read(buffer8)
+			instruction.modrm = buffer8[0]
+			instruction.opcodeString = selectTypeOfCalc(instruction.modrm)
+			instruction.modrmStruct = analyzeModRM(instruction.modrm)
+			instruction.address.segmentRegsiter = instruction.modrmStruct.mname
+			var buffer32 = make([]byte, 4)
+			var buffer = ""
+			switch instruction.modrmStruct.mod {
+			case 0x0:
+				buffer = "[" + instruction.address.segmentRegsiter + "]"
+			case 0x1:
+				file.Read(buffer8)
+				instruction.address.displacement = string(buffer8)
+				buffer = "[" + instruction.address.segmentRegsiter + "+0x" + instruction.address.displacement + "]"
+				break
+			case 0x2:
+				file.Read(buffer32)
+				instruction.address.displacement = string(buffer32)
+				buffer = "[" + instruction.address.segmentRegsiter + "+0x" + instruction.address.displacement + "]"
+				break
+			case 0x3:
+				buffer = instruction.address.segmentRegsiter
+			default:
+				break
+			}
+			file.Read(buffer8)
+			instruction.immediate = string(buffer8)
+			fmt.Printf("%s %s, %s", instruction.opcodeString, buffer, instruction.immediate)
 			break
+
 		case 0x89:
-			mnemonic += "mov"
-			file.Read(buffer)
-			getMnemonicFromType(buffer, file, true)
+			file.Read(buffer8)
+			instruction.modrm = buffer8[0]
+			instruction.opcodeString = "mov"
+			instruction.modrmStruct = analyzeModRM(instruction.modrm)
+			instruction.address.segmentRegsiter = instruction.modrmStruct.mname
+			var buffer32 = make([]byte, 4)
+			var buffer = ""
+			switch instruction.modrmStruct.mod {
+			case 0x0:
+				buffer = "[" + instruction.address.segmentRegsiter + "]"
+			case 0x1:
+				file.Read(buffer8)
+				instruction.address.displacement = string(buffer8)
+				buffer = "[" + instruction.address.segmentRegsiter + "+0x" + instruction.address.displacement + "]"
+				break
+			case 0x2:
+				file.Read(buffer32)
+				instruction.address.displacement = string(buffer32)
+				buffer = "[" + instruction.address.segmentRegsiter + "+0x" + instruction.address.displacement + "]"
+				break
+			case 0x3:
+				buffer = instruction.address.segmentRegsiter
+			default:
+				break
+			}
+			fmt.Printf("%s %s, %s\n", instruction.opcodeString, buffer, instruction.modrmStruct.rname)
+			break
+
 		case 0x8b:
-			mnemonic += "mov"
-			file.Read(buffer)
-			getMnemonicFromType(buffer, file, false)
+			file.Read(buffer8)
+			instruction.modrm = buffer8[0]
+			instruction.opcodeString = "mov"
+			instruction.modrmStruct = analyzeModRM(instruction.modrm)
+			instruction.address.segmentRegsiter = instruction.modrmStruct.mname
+			var buffer32 = make([]byte, 4)
+			var buffer = ""
+			switch instruction.modrmStruct.mod {
+			case 0x0:
+				buffer = "[" + instruction.address.segmentRegsiter + "]"
+			case 0x1:
+				file.Read(buffer8)
+				instruction.address.displacement = string(buffer8)
+				buffer = "[" + instruction.address.segmentRegsiter + "+0x" + instruction.address.displacement + "]"
+				break
+			case 0x2:
+				file.Read(buffer32)
+				instruction.address.displacement = string(buffer32)
+				buffer = "[" + instruction.address.segmentRegsiter + "+0x" + instruction.address.displacement + "]"
+				break
+			case 0x3:
+				buffer = instruction.address.segmentRegsiter
+			default:
+				break
+			}
+			fmt.Printf("%s %s, %s\n", instruction.opcodeString, instruction.modrmStruct.rname, buffer)
+			break
+
+		case 0x90:
+			instruction.opcodeString = "nop"
+			fmt.Println(instruction.opcodeString)
+
 		case 0xc7:
-			mnemonic += "mov"
-			file.Read(buffer)
-			mnemonic += getMnemonicFromType(buffer, file, true)
-			//つんだ [register + immidiate],immidiate型に対応してない
+			file.Read(buffer8)
+			instruction.modrm = buffer8[0]
+			instruction.opcodeString = "mov"
+			instruction.modrmStruct = analyzeModRM(instruction.modrm)
+			instruction.address.segmentRegsiter = instruction.modrmStruct.mname
+			var buffer32 = make([]byte, 4)
+			var buffer = ""
+			switch instruction.modrmStruct.mod {
+			case 0x0:
+				buffer = "[" + instruction.address.segmentRegsiter + "]"
+			case 0x1:
+				file.Read(buffer8)
+				instruction.address.displacement = string(buffer8)
+				buffer = "[" + instruction.address.segmentRegsiter + "+0x" + instruction.address.displacement + "]"
+				break
+			case 0x2:
+				file.Read(buffer32)
+				instruction.address.displacement = string(buffer32)
+				buffer = "[" + instruction.address.segmentRegsiter + "+0x" + instruction.address.displacement + "]"
+				break
+			case 0x3:
+				buffer = instruction.address.segmentRegsiter
+			default:
+				break
+			}
+			file.Read(buffer32)
+			instruction.immediate = string(buffer32)
+			fmt.Printf("%s %s, %s\n", instruction.opcodeString, buffer, instruction.immediate)
+			break
 		}
 
 	}
